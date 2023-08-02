f7e1339c8b8fabac5d86399cdf54157fc978cf68
Rodrigo Santos <passinhorcs@hotmail.com>
2023-08-02 15:43:28 -0300

    first commit

---
+++ rocket movies/src/Components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./Styles";
+
+export function Button ({ title, loading}){
+  return(
+  <Container type="button"
+ 
+
+   >
+  </Container>
+ );
+}
---
+++ rocket movies/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+import { Button } from '../../Components/Button'
+export function Details () {
+  return (
+    <Container>
+      < Button title="Enter" loading />
+      < Button  title="Login"/>
+      < Button title= "Logout"/>
+    </Container>
+  )
+}
+
+
