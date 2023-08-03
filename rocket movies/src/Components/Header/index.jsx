3ec975b83b83e0327fa88f97108fba646720ad8a
Rodrigo Santos <passinhorcs@hotmail.com>
2023-08-03 15:18:23 -0300

    header component

---
+++ rocket movies/src/Components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+export function Header (){
+   return(
+    <Container>
+      <Profile>
+         <img src="https://github.com/rodrigosantos0660"
+         alt= "picture of user" />
+      </Profile>
+      
+    </Container>
+ )
+}
