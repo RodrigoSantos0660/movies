6772ca2a7657cac36e0d505215e052b0473c6a5a
Rodrigo Santos <passinhorcs@hotmail.com>
2023-09-06 16:31:24 -0300

    creating page preview

---
+++ rocket movies/src/pages/MoviePreview/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5% 0px;
+  flex-direction: column;
+
+  border: 1px solid white
+  
+
+
+`;
